ce65b150e7d3f92f47e3953318b088207d1cd7eb
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-09 00:09:15 +0100

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,56 @@
-import React from 'react'
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
+import React, { useEffect, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 export const Hero = () => {
+    const [videoScr, setVideoScr] = useState(
+        window.innerWidth < 760 ? smallHeroVideo : heroVideo
+    );
+    const handleVideoScrSet = () => {
+        if (window.innerWidth < 760) {
+            setVideoScr(smallHeroVideo)
+        } else {
+            setVideoScr(heroVideo)
+        }
+    }
 
-    useGSAP(() => {
-        gsap.to('#hero', {
-            opacity: 1,
-            delay: 1.5
-        })
+    useEffect(() => {
+        window.addEventListener('resize', handleVideoScrSet);
+
+        return () => {
+            window.removeEventListener('resize', handleVideoScrSet)
+        }
     }, [])
 
+    useGSAP(() => {
+        gsap.to("#hero", {
+            opacity: 1,
+            delay: 2,
+        });
+        gsap.to("#cta", {
+            opacity: 1,
+            y: -50,
+            delay: 2,
+        });
+    }, []);
+
     return (
-        <section className='w-full nav-height bg-black relative'>
-            <div className='h-5/6 w-full flex-center flex-col'>
-                <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+        <section className="w-full nav-height bg-black relative">
+            <div className="h-5/6 w-full flex-center flex-col">
+                <p id="hero" className="hero-title">
+                    iPhone 15 Pro
+                </p>
+                <div className="md:w-10/12 w-9/12">
+                    <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoScr}>
+                        <source src={videoScr} type="video/mp4" />
+                    </video>
+                </div>
+            </div>
+            <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+                <a href="#highlights" className="btn">Buy</a>
+                <p className="font-normal text-xl">From $199/month or $999</p>
             </div>
         </section>
-    )
-}
+    );
+};
