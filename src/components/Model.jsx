657db5f1c2021c3a1118ac59517b9859337733a0
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-10 23:16:03 +0100

    model sizes

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants";
+import { models, sizes } from "../constants";
 
 export const Model = () => {
     const [size, setSize] = useState("small");
@@ -86,12 +86,28 @@ export const Model = () => {
                                 {models.map((item, index) => (
                                     <li
                                         key={index}
-                                        className="w-6 h-6 rounded-full mx-2"
+                                        className="w-6 h-6 rounded-full mx-2 cursor-pointer"
                                         style={{ backgroundColor: item.color[0] }}
                                         onClick={() => setModel(item)}
                                     />
                                 ))}
                             </ul>
+
+                            <button className="size-btn-container">
+                                {sizes.map(({ label, value }) => (
+                                    <span
+                                        key={label}
+                                        className="size-btn"
+                                        style={{
+                                            backgroundColor: size === value ? "white" : "transparent",
+                                            color: size === value ? "black" : "white",
+                                        }}
+                                        onClick={() => setSize(value)}
+                                    >
+                                        {label}
+                                    </span>
+                                ))}
+                            </button>
                         </div>
                     </div>
                 </div>
