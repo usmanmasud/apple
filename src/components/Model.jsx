75854a62113ea9b86d9c7b861c60b13dda1c830c
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-10 23:04:11 +0100

    models color

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,22 +2,31 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useRef, useState } from "react";
 import { ModelView } from "./ModelView";
-import { yellowImg } from '../utils'
-import * as THREE from 'three'
+import { yellowImg } from "../utils";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models } from "../constants";
 
 export const Model = () => {
-    const [size, setSize] = useState('small');
+    const [size, setSize] = useState("small");
     const [model, setModel] = useState({
-        title: 'iPhone 15 Pro in Natural Titnium',
-        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
-        img: yellowImg
-    })
+        title: "iPhone 15 Pro in Natural Titnium",
+        color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+        img: yellowImg,
+    });
 
     // camera control for the model view
     const cameraControlSmall = useRef();
     const cameraControlLarge = useRef();
 
+    // models
     const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
+
+    // rotation
+    const [smallRotation, setSmallRotation] = useState(0);
+    const [largeRotation, setLargeRotation] = useState(0);
 
     useGSAP(() => {
         gsap.to("#heading", {
@@ -34,7 +43,56 @@ export const Model = () => {
                 </h1>
                 <div className="flex flex-col items-center mt-5">
                     <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                        <ModelView />
+                        <ModelView
+                            index={1}
+                            groupRef={small}
+                            gsapType="view1"
+                            controlRef={cameraControlSmall}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
+                        />
+                        <ModelView
+                            index={2}
+                            groupRef={large}
+                            gsapType="view2"
+                            controlRef={cameraControlLarge}
+                            setRotationState={setLargeRotation}
+                            item={model}
+                            size={size}
+                        />
+
+                        <Canvas
+                            className="w-full h-full"
+                            style={{
+                                position: "fixed",
+                                top: 0,
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                overflow: "hidden",
+                            }}
+                            eventSource={document.getElementById("root")}
+                        >
+                            <View.Port />
+                        </Canvas>
+                    </div>
+
+                    <div className="mx-auto w-full">
+                        <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
+                        <div className="flex-center">
+                            <ul className="color-container">
+                                {models.map((item, index) => (
+                                    <li
+                                        key={index}
+                                        className="w-6 h-6 rounded-full mx-2"
+                                        style={{ backgroundColor: item.color[0] }}
+                                        onClick={() => setModel(item)}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
