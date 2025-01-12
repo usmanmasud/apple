df0b05699f48ee45baa29797ea62ba5c550647ff
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-12 23:47:51 +0100

    animate with gsap reusable function

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 export const animateWithGapTimeline = (
   timeline,
   rotationRef,
@@ -29,4 +31,16 @@ export const animateWithGapTimeline = (
     },
     "<"
   );
+};
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
 };
