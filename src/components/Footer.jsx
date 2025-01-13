c5b3c65a3877b645bd5ec6307c619990a5f38e1f
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-13 01:01:30 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { footerLinks } from "../constants";
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
-    )
-}
+        <footer className="py-5 sm:px-10 px-5">
+            <div className="screen-max-width">
+                <div className="font-semibold text-gray text-xs">
+                    <p>
+                        More ways to shop:{" "}
+                        <span className="underline text-blue">Find an Apple Store </span>
+                        or {""}
+                        <span className="underline text-blue">other retailer</span> near
+                        you.
+                    </p>
+                    <p>Or call 08080574228</p>
+                </div>
+                <div className="bg-neutral-700 my-5 h-[1px]" />
+
+                <div className="flex md:flex-row flex-col md:items-center justify-between">
+                    <p className="font-semibold text-gray text-xs">
+                        Copyright @ 2025 Apple Inc. All rights reserved.
+                    </p>
+                    <div className="flex">
+                        {footerLinks.map((link, i) => (
+                            <p key={link} className="font-semibold text-gray text-xs">
+                                {link} {' '}
+                                {i !== footerLinks.length - 1 && <span className="mx-2"> |</span>}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
